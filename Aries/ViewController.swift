1bb15d545d4b53838811852dfeada241728372d3
olaade27 <oadeoba1@myseneca.ca>
2024-05-01 18:30:55 -0400

    Initial Commit

---
+++ Aries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aries
+//
+//  Created by Ola Adeoba on 2024-05-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
